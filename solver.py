dde220ea174d5d0ba574349e86354d482f92a8a4
Marek Choiński <marekchoinski17@gmail.com>
2018-06-16 17:50:08 +0200

    I said 45 minutes
    also I listened to teacher to learn advanced C++98

--- solver.py
+++ solver.py
@@ -2,25 +2,21 @@ import numpy as np
 from collections import deque
 
 labirynth = [
-['#','#','#','#','#','#','#'],
-['#','S',' ',' ',' ',' ','#'],
-['#',' ','#',' ','#','#','#'],
-['#',' ','#',' ','#',' ','#'],
-['#',' ','#','#','#',' ','#'],
-['#',' ',' ',' ',' ','E','#'],
-['#','#','#','#','#','#','#'],
-
+    ['#','#','#','#','#','#','#'],
+    ['#','S',' ',' ',' ',' ','#'],
+    ['#',' ','#',' ','#','#','#'],
+    ['#',' ','#',' ','#',' ','#'],
+    ['#',' ','#','#','#',' ','#'],
+    ['#',' ',' ',' ',' ','E','#'],
+    ['#','#','#','#','#','#','#'],
 ]
 
 size = 7
 
-
-# #######
-
 # find end and start
 # we search for S and E
 
-#indexes of S and E
+# indexes of S and E
 start = (-1, -1)
 end = (-1, -1)
 
@@ -31,20 +27,18 @@ for i, row in enumerate(labirynth):
         if x == 'E':
             end = (i, j)
 
-#change E to free field
+# change E to free field
 
 labirynth[end[0]][end[1]] = ' '
 
 
 print(start, end)
 
-#make queue
+# make queue
 
 queue = deque()
 
 queue.append(start)
-
-#
 
 find = False
 
@@ -80,9 +74,42 @@ while len(queue) != 0:
         queue.append(temp)
 
 
+# go through solution
+
+    shortest_path = end
+    if labirynth[shortest_path[0]][shortest_path[1]] == ' ':
+        is_solved = False
+    else:
+        is_solved = True
+
+        while labirynth[shortest_path[0]][shortest_path[1]] != 'S':
+
+            if labirynth[shortest_path[0]][shortest_path[1]] == 'u':
+                labirynth[shortest_path[0]][shortest_path[1]] = 'U'
+                shortest_path = (shortest_path[0]-1, shortest_path[1])
+
+            if labirynth[shortest_path[0]][shortest_path[1]] == 'd':
+                labirynth[shortest_path[0]][shortest_path[1]] = 'D'
+                shortest_path = (shortest_path[0]+1, shortest_path[1])
+
+            if labirynth[shortest_path[0]][shortest_path[1]] == 'l':
+                labirynth[shortest_path[0]][shortest_path[1]] = 'L'
+                shortest_path = (shortest_path[0], shortest_path[1]-1)
+
+            if labirynth[shortest_path[0]][shortest_path[1]] == 'r':
+                labirynth[shortest_path[0]][shortest_path[1]] = 'R'
+                shortest_path = (shortest_path[0], shortest_path[1]+1)
 
 
+labirynth[end[0]][end[1]] = 'E'
+
+
+'''
 for row in labirynth:
     for x in row:
         print(x, end='')
     print('\n')
+
+
+print(is_solved)
+'''
