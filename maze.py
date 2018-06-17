d8d99b2097ed8269953712e5ca9d5934faa91839
Marek Choiński <marekchoinski17@gmail.com>
2018-06-17 21:04:37 +0200

    Find position of end points

--- maze.py
+++ maze.py
@@ -8,12 +8,16 @@ cap = cv2.VideoCapture(0)
 def mask_to_text(mask):
     result = [[None] * len(mask[0])] * len(mask)
 
+
+
     for row_num, row in enumerate(mask):
         for pixel_num, pixel in enumerate(row):
             if pixel == 0:
                 result[row_num][pixel_num] = ' '
             else:
                 result[row_num][pixel_num] = '#'
+
+    print(result)
 
     return result
 
@@ -22,12 +26,9 @@ def find_largest_groups_in_mask(mask):
     pass
 
 
-def detect_end_points(frame):
+def show_end_points(frame):
     # save normal frame
     original_frame = frame
-
-    # take  e d g y  frames
-    # edges1 = cv2.Canny(frame, 100, 200)
 
     # convert color scale to HSV
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -39,18 +40,47 @@ def detect_end_points(frame):
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(frame, lower_green, upper_green)
 
-    # dilate final pieces for better effect
+    # dilate for better effect
     kernel = np.ones((3, 3), np.uint8)
-    # final_edges = cv2.dilate(final_edges, kernel, iterations=1)
     mask = cv2.dilate(mask, kernel, iterations=2)
-
-    # substrate walls with start and end
-    # final_maze_masked = final_edges - final_mask
 
     backtorgb = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
 
     # original frame is overlayed by detected path
     return cv2.addWeighted(original_frame, 1.0, backtorgb, 0.1, 0)
+
+
+def find_the_biggest(image, mask):
+    output = cv2.bitwise_and(image, image, mask=mask)
+
+    ret, thresh = cv2.threshold(mask, 40, 255, 0)
+    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    print(len(contours))
+
+    if len(contours) >= 2:
+        # draw in blue the contours that were founded
+        cv2.drawContours(output, contours, -1, 255, 3)
+
+        # find the biggest area
+        c = max(contours, key=cv2.contourArea)
+
+        x, y, w, h = cv2.boundingRect(c)
+        # draw the book contour (in green)
+        cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        contours.remove(c)
+
+        # find the biggest area
+        c = max(contours, key=cv2.contourArea)
+
+        x, y, w, h = cv2.boundingRect(c)
+        # draw the book contour (in green)
+        cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # show the images
+    cv2.imshow("Result", np.hstack([image, output]))
+
+    cv2.waitKey(0)
 
 
 def mazeMagic():
@@ -101,6 +131,9 @@ def mazeMagic():
     # original frame is overlayed by detected path
     original_frame = cv2.addWeighted(original_frame, 1.0, backtorgb, 0.1, 0)
 
+    # mask_to_text(final_mask)
+    find_the_biggest(original_frame, final_mask)
+
     # show windows
     cv2.imshow('mask', final_mask)
     cv2.imshow('mask2', original_frame)
@@ -109,15 +142,6 @@ def mazeMagic():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-
-'''
-
-
-    final_maze_masked = cv2.imread(final_maze_masked, 0)
-    final_maze_masked = cv2.cvtColor(final_maze_masked, cv2.COLOR_BGR2GRAY)
-    img_expanded = final_maze_masked[:, :, np.newaxis]
-'''
-# cv2.imshow('edges_masked2', img_expanded)
 
 
 if __name__ == '__main__':
@@ -131,7 +155,7 @@ if __name__ == '__main__':
             # edges detection - used to extract the maze shape
             edges = cv2.Canny(frame, 200, 200)
 
-            frame = detect_end_points(frame)
+            frame = show_end_points(frame)
 
             cv2.imshow('frame', frame)
             # cv2.imshow('edges', edges)
@@ -144,3 +168,8 @@ if __name__ == '__main__':
                 cv2.destroyAllWindows()
                 mazeMagic()
                 break
+            elif k == ord('t'):
+                cv2.destroyAllWindows()
+                mazeMagic()
+                break
+
