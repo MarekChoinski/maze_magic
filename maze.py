f462da304b7eef01d8870574c02fee3197a01b5a
MarekChoinski <marekchoinski@gmail.com>
2018-07-18 11:45:42 +0200

    Performance improved

--- maze.py
+++ maze.py
@@ -7,142 +7,124 @@ import threading
 
 
 class Maze:
-    __lower_green = np.array([40, 100, 85])
-    __upper_green = np.array([75, 255, 255])
+    _lower_green = np.array([40, 100, 85])
+    _upper_green = np.array([75, 255, 255])
 
-    __kernel = np.ones((3, 3), np.uint8)
-
+    _kernel = np.ones((3, 3), np.uint8)
 
     def __init__(self, cap, title):
 
-        _,self.__frame = cap.read()
+        _, self._frame = cap.read()
 
-        self.__maze_mask = np.zeros((len(self.__frame), len(self.__frame[0]), 3), np.uint8)
-        self.__circles = np.zeros((len(self.__frame), len(self.__frame[0]), 3), np.uint8)
-        self.__blank_image = np.zeros((len(self.__frame), len(self.__frame[0]), 3), np.uint8)
+        self._maze_mask = np.zeros((len(self._frame), len(self._frame[0]), 3), np.uint8)
+        self._lab_mask = np.zeros((len(self._frame), len(self._frame[0]), 3), np.uint8)
+        self._circles = np.zeros((len(self._frame), len(self._frame[0]), 3), np.uint8)
+        # self._blank_image = np.zeros((len(self._frame), len(self._frame[0]), 3), np.uint8)
 
-        self.__thread_running = True
+        self._sensitivity = 90
 
-        maze_calculation_thread = threading.Thread(target=self.__maze_calculation)
-        draw_end_circles_thread = threading.Thread(target=self.__show_end_points)
+        self._thread_running = True
+
+
+
+        maze_calculation_thread = threading.Thread(target=self._maze_calculation)
 
         maze_calculation_thread.start()
-        draw_end_circles_thread.start()
 
         while True:
+            _, self._frame = cap.read()
 
-            _, self.__frame = cap.read()
+            actual_frame = self._frame
 
-            actual_frame = self.__frame
-
-            # frame = show_end_points(globals.frame1)
-
-            actual_frame = cv2.addWeighted(actual_frame, 1, self.__maze_mask, 0.5, 0)
-            actual_frame = cv2.addWeighted(actual_frame, 1, self.__circles, 0.3, 0)
+            # circles
+            actual_frame = cv2.addWeighted(actual_frame, 1, self._maze_mask, 0.5, 0)
+            actual_frame = cv2.addWeighted(actual_frame, 1, self._circles, 0.3, 0)
 
             cv2.imshow(title, actual_frame)
 
             k = cv2.waitKey(5) & 0xFF
             if k == ord('q'):
-                self.__thread_running = False
+                self._thread_running = False
                 maze_calculation_thread.join()
-                draw_end_circles_thread.join()
                 cv2.destroyAllWindows()
                 break
 
-    def __mask_to_text(self, mask):
-        result = [['x' for x in range(len(mask[0]))] for y in range(len(mask))]
-
-        for row_num, row in enumerate(mask):
-            for pixel_num, pixel in enumerate(row):
-                if pixel > 250:
-                    result[row_num][pixel_num] = '#'
-                else:
-                    result[row_num][pixel_num] = ' '
-
-        return result
-
-    def __show_end_points(self):
-        while self.__thread_running:
-
-            # save normal frame
-            frame = copy.copy(self.__frame)
-
-            # convert color scale to HSV
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-            # Threshold the HSV image to get only blue colors
-            mask = cv2.inRange(frame, self.__lower_green, self.__upper_green)
-
-            # dilate for better effect
-            mask = cv2.dilate(mask, self.__kernel, iterations=2)
-
-            positions = self.__position_of_end_points(mask)
-
-            if len(positions) == 2:
-                self.__circles = copy.copy(self.__blank_image)
-                copy_frame = self.__circles
-                cv2.circle(copy_frame, (positions[0][0], positions[0][1]), positions[0][2], (0, 255, 0), -1)
-                cv2.circle(copy_frame, (positions[1][0], positions[1][1]), positions[1][2], (0, 255, 0), -1)
 
     # we should find position of end points
     # there could be a lot of green elements on frame
     # so we should find two the biggest
     # returns position of start and end and diameter of found point
-    def __position_of_end_points(self, mask):
-        # idk
-        ret, thresh = cv2.threshold(mask, 40, 255, 0)
-        # idk
+    def _position_of_end_points(self, mask):
+        r, thresh = cv2.threshold(mask, 40, 255, 0)
         im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
         positions = []
 
         if len(contours) >= 2:
-            sorted_contours = sorted(contours, key=cv2.contourArea)
+            contours.sort(key=cv2.contourArea, reverse=True)
 
-            # find the biggest area point
-            x, y, w, h = cv2.boundingRect(sorted_contours[-2])
-            positions.append((int(round(x + (w / 2))), int(round(y + (h / 2))), int(round(max(w, h)))))
-            x, y, w, h = cv2.boundingRect(sorted_contours[-1])
-            positions.append((int(round(x + (w / 2))), int(round(y + (h / 2))), int(round(max(w, h)))))
+            for i, con in enumerate(contours):
+                if i == 0:
+                    x, y, w, h = cv2.boundingRect(con)
+                    positions.append((int(round(x + (w / 2))),  # center
+                                      int(round(y + (h / 2))),
+                                      int(round(max(w, h)))))
+                    bx, by, bh = x, y, max(w, h)
+
+                else:
+                    x, y, w, h = cv2.boundingRect(con)
+
+                    if (x>=bx and x<bx+bh) or (x>=by and x<by+bh):
+                            break
+                    positions.append((int(round(x + (w / 2))), # center
+                                      int(round(y + (h / 2))),
+                                      int(round(max(w, h)))))
+                    break
+
+
 
         return positions
 
-    def __draw_path_on_frame(self, path):
-        blank_image = copy.copy(self.__blank_image)
+    def _draw_path_on_frame(self, path):
+        blank_image = np.zeros((len(self._frame), len(self._frame[0]), 3), np.uint8)
         for p in path:
-            blank_image[p[0]][p[1]] = (255, 255, 255)
+            blank_image[p] = (255, 255, 255)
 
         frame = blank_image
 
         return frame
 
-    def __maze_calculation(self):
-        print("a")
-        while self.__thread_running:
-            # take  e d g y  frames
-            edges1 = cv2.Canny(self.__frame, 100, 200)
 
-            # convert color scale to HSV
-            copied_frame = cv2.cvtColor(self.__frame, cv2.COLOR_BGR2HSV)
+    def _maze_calculation(self):
+        while self._thread_running:
+
+            #self._circles = np.zeros((len(self._frame), len(self._frame[0]), 3), np.uint8)
+            #self._maze_mask = np.zeros((len(self._frame), len(self._frame[0]), 3), np.uint8)
+
+            # gray
+            gray_frame = cv2.cvtColor(self._frame, cv2.COLOR_BGR2GRAY)
+            r, labirynth_mask = cv2.threshold(gray_frame, self._sensitivity, 255, cv2.THRESH_BINARY_INV)
 
             # Threshold the HSV image to get only blue colors
-            mask1 = cv2.inRange(copied_frame, self.__lower_green, self.__upper_green)
+            hsv_frame = cv2.cvtColor(self._frame, cv2.COLOR_BGR2HSV)
+            points_mask = cv2.inRange(hsv_frame, self._lower_green, self._upper_green)
 
-            # connect all pieces to one
-            final_edges = edges1
-            final_mask = mask1
+
 
             # dilate final pieces for better effect
-            final_edges = cv2.dilate(final_edges, self.__kernel, iterations=1)
-            final_mask = cv2.dilate(final_mask, self.__kernel, iterations=2)
+            labirynth_mask = cv2.dilate(labirynth_mask, self._kernel, iterations=2)
+            points_mask = cv2.dilate(points_mask, self._kernel, iterations=2)
 
-            final_maze_masked = final_edges - final_mask
+            # connect all pieces to ones
+            final_maze_masked = labirynth_mask - points_mask
 
-            positions = self.__position_of_end_points(mask1)
+            positions = self._position_of_end_points(points_mask)
 
             if len(positions) == 2:
-                labirynth = self.__mask_to_text(final_maze_masked)
-                path = solver.solve(labirynth, positions[0], positions[1])
-                self.__maze_mask = self.__draw_path_on_frame(path)
+                self._circles = np.zeros((len(self._frame), len(self._frame[0]), 3), np.uint8)
+                cv2.circle(self._circles, (positions[0][0], positions[0][1]), positions[0][2], (0, 255, 0), -1)
+                cv2.circle(self._circles, (positions[1][0], positions[1][1]), positions[1][2], (0, 255, 0), -1)
 
+                path = solver.solve(final_maze_masked, positions[1], positions[0])
+                if len(path) > 2:
+                    self._maze_mask = self._draw_path_on_frame(path)
--- solver.py
+++ solver.py
@@ -1,9 +1,28 @@
 import numpy as np
 from collections import deque
 
+class Direction:
+    WALL = 255
+    WAY = 0
+
+    START = 1
+    END = 2
+
+    UP = 3
+    DOWN = 4
+    LEFT = 5
+    RIGHT = 6
+
+    UP_FOUND = 7
+    DOWN_FOUND = 8
+    LEFT_FOUND = 9
+    RIGHT_FOUND = 10
+
+
 
 def solve(labirynth, i_start, i_end):
     path = []
+    labirynth = np.pad(labirynth, pad_width=0, mode='constant', constant_values=255)
 
     # start = i_start
     # end = i_end
@@ -14,10 +33,10 @@ def solve(labirynth, i_start, i_end):
     path.append(start)
     path.append(end)
 
-    labirynth[start[0]][start[1]] = 'S'
-    labirynth[end[0]][end[1]] = 'E'
+    labirynth[start[0]][start[1]] = Direction.START
+    #labirynth[end[0]][end[1]] = Direction.END
 
-    labirynth[end[0]][end[1]] = ' '
+    #labirynth[end[0]][end[1]] = Direction.WAY
 
     queue = deque()
 
@@ -31,29 +50,28 @@ def solve(labirynth, i_start, i_end):
         if index == end:
             find = True
             break
-
         try:
             # up
-            if labirynth[index[0]][index[1] - 1] == ' ':
-                labirynth[index[0]][index[1] - 1] = 'd'
+            if labirynth[index[0]][index[1] - 1] == Direction.WAY:
+                labirynth[index[0]][index[1] - 1] = Direction.DOWN
                 temp = (index[0], index[1] - 1)
                 queue.append(temp)
 
             # down
-            if labirynth[index[0]][index[1] + 1] == ' ':
-                labirynth[index[0]][index[1] + 1] = 'u'
+            if labirynth[index[0]][index[1] + 1] == Direction.WAY:
+                labirynth[index[0]][index[1] + 1] = Direction.UP
                 temp = (index[0], index[1] + 1)
                 queue.append(temp)
 
             # left
-            if labirynth[index[0] - 1][index[1]] == ' ':
-                labirynth[index[0] - 1][index[1]] = 'r'
+            if labirynth[index[0] - 1][index[1]] == Direction.WAY:
+                labirynth[index[0] - 1][index[1]] = Direction.RIGHT
                 temp = (index[0] - 1, index[1])
                 queue.append(temp)
 
             # right
-            if labirynth[index[0] + 1][index[1]] == ' ':
-                labirynth[index[0] + 1][index[1]] = 'l'
+            if labirynth[index[0] + 1][index[1]] == Direction.WAY:
+                labirynth[index[0] + 1][index[1]] = Direction.LEFT
                 temp = (index[0] + 1, index[1])
                 queue.append(temp)
         except IndexError:
@@ -64,34 +82,33 @@ def solve(labirynth, i_start, i_end):
 
     shortest_path = end
     try:
-        if labirynth[shortest_path[0]][shortest_path[1]] == ' ':
+        if labirynth[shortest_path] == Direction.WAY:
             is_solved = False
         else:
             is_solved = True
 
             path.append(shortest_path)
 
-            while labirynth[shortest_path[0]][shortest_path[1]] != 'S':
-                # print("paf: ",shortest_path, labirynth[shortest_path[0]][shortest_path[1]])
+            while labirynth[shortest_path[0]][shortest_path[1]] != Direction.START:
 
-                if labirynth[shortest_path[0]][shortest_path[1]] == 'u':
+                if labirynth[shortest_path] == Direction.UP:
                     path.append(shortest_path)
-                    labirynth[shortest_path[0]][shortest_path[1]] = 'U'
+                    labirynth[shortest_path] = Direction.UP_FOUND
                     shortest_path = (shortest_path[0], shortest_path[1] - 1)
 
-                if labirynth[shortest_path[0]][shortest_path[1]] == 'd':
+                if labirynth[shortest_path] == Direction.DOWN:
                     path.append(shortest_path)
-                    labirynth[shortest_path[0]][shortest_path[1]] = 'D'
+                    labirynth[shortest_path] = Direction.DOWN_FOUND
                     shortest_path = (shortest_path[0], shortest_path[1] + 1)
 
-                if labirynth[shortest_path[0]][shortest_path[1]] == 'l':
+                if labirynth[shortest_path] == Direction.LEFT:
                     path.append(shortest_path)
-                    labirynth[shortest_path[0]][shortest_path[1]] = 'L'
+                    labirynth[shortest_path] = Direction.LEFT_FOUND
                     shortest_path = (shortest_path[0] - 1, shortest_path[1])
 
-                if labirynth[shortest_path[0]][shortest_path[1]] == 'r':
+                if labirynth[shortest_path] == Direction.RIGHT:
                     path.append(shortest_path)
-                    labirynth[shortest_path[0]][shortest_path[1]] = 'R'
+                    labirynth[shortest_path] = Direction.RIGHT_FOUND
                     shortest_path = (shortest_path[0] + 1, shortest_path[1])
     except IndexError:
         return path
